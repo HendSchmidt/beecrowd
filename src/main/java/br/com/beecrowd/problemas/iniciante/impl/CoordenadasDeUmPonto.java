9cd4d03c02a2bc3f690c4232e9b0bde7a05210f4
Hendrix Schmidt <hendrixschmidt@hotmail.com>
2025-10-17 19:37:01 -0300

    refactor problem

--- src/main/java/br/com/beecrowd/problemas/iniciante/impl/CoordenadasDeUmPonto.java
+++ src/main/java/br/com/beecrowd/problemas/iniciante/impl/CoordenadasDeUmPonto.java
@@ -1,38 +1,47 @@
 package br.com.beecrowd.problemas.iniciante.impl;
 
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import br.com.beecrowd.problemas.iniciante.Executavel;
 
-public class CoordenadasDeUmPonto implements Executavel{
+public class CoordenadasDeUmPonto implements Executavel {
+    private static final Map<BiPredicate<Double, Double>, String> ESTRATEGIAS = Map.ofEntries(
+            Map.entry((BiPredicate<Double, Double>) (x, y) -> x > 0 && y > 0, "Q1"),
+            Map.entry((BiPredicate<Double, Double>) (x, y) -> x < 0 && y < 0, "Q2"),
+            Map.entry((BiPredicate<Double, Double>) (x, y) -> x < 0 && y > 0, "Q3"),
+            Map.entry((BiPredicate<Double, Double>) (x, y) -> x > 0 && y < 0, "Q4"));
 
     @Override
     public String execute() {
-       return calculaQuadrante(input());
+        return calculaQuadrante(input());
     }
 
-    private String calculaQuadrante(Optional<Coordenadas> coordenadas){
+    private String calculaQuadrante(Coordenadas coordenadas) {
         StringBuilder resultado = new StringBuilder();
-        coordenadas.ifPresent(coordenada -> {
-            if(coordenada.x > 0 && coordenada.y > 0){resultado.append("Q1").toString();}
-            if(coordenada.x > 0 && coordenada.y < 0){resultado.append("Q4").toString();}
-            if(coordenada.x < 0 && coordenada.y < 0){resultado.append("Q2").toString();}
-            if(coordenada.x < 0 && coordenada.y > 0){resultado.append("Q3").toString();}
-        });
 
-            
-
-        return resultado.isEmpty() ? resultado.append("Origem").toString() : resultado.toString();
+        return resultado
+                .append(ESTRATEGIAS.entrySet()
+                        .stream()
+                        .filter(estrategia -> estrategia.getKey().test(coordenadas.x, coordenadas.y))
+                        .findFirst()
+                        .map(Map.Entry::getValue)
+                        .map(Function.identity())
+                        .orElse("Origem"))
+                .toString();
     }
 
-    private Optional<Coordenadas> input(){
+    private Coordenadas input() {
         try (Scanner in = new Scanner(System.in)) {
             in.useLocale(Locale.US);
-            return Optional.of(new Coordenadas(in.nextDouble(), in.nextDouble()));
-        } 
+            return new Coordenadas(in.nextDouble(), in.nextDouble());
+        }
     }
 
-    private record Coordenadas(double x, double y){}
+    private record Coordenadas(double x, double y) {
+    }
 }
